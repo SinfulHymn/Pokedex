0a612d2698815d55f1c8fd0cd2e41a70b59d174f
Alejandro Rojas <kmachappy@pm.me>
2022-04-16 16:42:15 -0700

    rewrote new.ejs and index.ejs

---
+++ public/script.js
@@ -0,0 +1,19 @@
+
+$(".type:contains(Grass)").css({"color": "#7AC74C"});
+$(".type:contains(Poison)").css({ "color": "#A33EA1"});
+$(".type:contains(Fire)").css({ "color": "#EE8130"});
+$(".type:contains(Flying)").css({ "color": "#A98FF3"});
+$(".type:contains(Water)").css({"color": "#6390F0"});
+$(".type:contains(Bug)").css({ "color": "#A6B91A"});
+$(".type:contains(Normal)").css({ "color": "#A8A77A"});
+$(".type:contains(Electric)").css({ "color": "#F7D02C"});
+$(".type:contains(Ice)").css({ "color": "#96D9D6", });
+$(".type:contains(Fighting)").css({ "color": "#C22E28"});
+$(".type:contains(Ground)").css({ "color": "#E2BF65" });
+$(".type:contains(Psychic)").css({ "color": "#F95587" }); 
+$(".type:contains(Rock)").css({ "color": "#B6A136" });
+$(".type:contains(Ghost)").css({ "color": "#735746" });
+$(".type:contains(Dark)").css({ "color": "#705746" });
+$(".type:contains(Steel)").css({ "color": "#B7B7CE",  });
+$(".type:contains(Fairy)").css({ "color": "#D685AD",  });
+$("h5:contains(Dragon)").css({ "color": "#6F35FC"});
--- server.js
+++ server.js
@@ -1,66 +1,71 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3001
-const pokemon = require("./models/pokemon.js")
-const morgan = require('morgan')
-const methodOverride = require('method-override')
-const req = require("express/lib/request")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const pokemon = require("./models/pokemon.js");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const req = require("express/lib/request");
 
-app.use(express.urlencoded({ extended: false }))
-app.use(morgan('tiny'))
-app.use("/static", express.static("public"))
-app.use(methodOverride("_method"))
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+app.use("/static", express.static("public"));
+app.use(methodOverride("_method"));
 
-
+//home
 app.get("/", (req, res) => {
-    res.render("home.ejs")
-})
-
+  res.render("home.ejs");
+});
+//index
 app.get("/pokemon/", (req, res) => {
-    res.render("index.ejs", { allPokemon: pokemon })
-})
+  res.render("index.ejs", { allPokemon: pokemon });
+});
 
+//new
 app.get("/pokemon/new", (req, res) => {
-    res.render("new.ejs")
-})
+  res.render("new.ejs", {
+    pokemon: pokemon[1],
+    index: 1,
+  });
+});
 
+//show
 app.get("/pokemon/:id", (req, res) => {
-    res.render("show.ejs", { pokemon: pokemon[req.params.id] })
-})
+  res.render("show.ejs", {
+    pokemon: pokemon[req.params.id],
+    index: req.params.id,
+  });
+});
+//edit
+app.get("/pokemon/:id/edit", (req, res) => {
+  res.render("edit.ejs", {
+    pokemon: pokemon[req.params.id],
+    index: req.params.id,
+  });
+});
 
+//create
 app.post("/pokemon", (req, res) => {
-    pokemon.push(req.body)
+    console.log(req.body)
+  pokemon.push(req.body);
 
-    res.redirect("/pokemon")
-})
+  res.redirect("/pokemon");
+});
 
+//delete
 app.delete("/pokemon/:id", (req, res) => {
-    const index = req.params.id
+  const index = req.params.id;
 
-    pokemon.splice(index, 1)
-    res.redirect('/pokemon')
-})
+  pokemon.splice(index, 1);
+  res.redirect("/pokemon");
+});
 
-app.get("/pokemon/:id/edit",(req,res)=>{
-    res.render("edit.ejs",{
-        pokemon: pokemon[req.params.id],
-        index: req.params.id
-    })
-})
-
-app.put("pokemon/:id", (req,res)=>{
-    
-})
-
-
-
-
-
+app.put("pokemon/:id", (req, res) => {
+  console.log(req.body);
+}); 
 
 app.listen(PORT, () => {
-    console.log(`We are listening on port ${PORT}`)
-})
-
-
+  console.log(`We are listening on port ${PORT}`);
+});
+ 
